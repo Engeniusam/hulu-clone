e5bb15892e533e8cbfe8354af1cce0a297176374
Engeniusam <engeniusam@gmail.com>
2022-06-27 18:19:53 +0300

    Updates

---
+++ precache-manifest.457934842a23caaa7ffd6a81a8fe1b22.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d1b6d38ec6b99f045ba",
+    "url": "/hulu-clone/static/css/main.65268988.chunk.css"
+  },
+  {
+    "revision": "6d1b6d38ec6b99f045ba",
+    "url": "/hulu-clone/static/js/main.6d1b6d38.chunk.js"
+  },
+  {
+    "revision": "05f317640d51b82fd187",
+    "url": "/hulu-clone/static/js/1.05f31764.chunk.js"
+  },
+  {
+    "revision": "04465e25e1fc8346ab85",
+    "url": "/hulu-clone/static/js/2.04465e25.chunk.js"
+  },
+  {
+    "revision": "99b4b9a6cf0e80cdad41",
+    "url": "/hulu-clone/static/js/runtime~main.99b4b9a6.js"
+  },
+  {
+    "revision": "63df8c19f798d196f0a0a3b8db3f795c",
+    "url": "/hulu-clone/static/media/hulu.63df8c19.png"
+  },
+  {
+    "revision": "edb3a65478c6ee04df7e31c8cebc73c7",
+    "url": "/hulu-clone/index.html"
+  }
+];
